48f37e1c2786bc239fe5d1c104b2d17c88053042
rsvasanth <rsvasanth@gmail.com>
2023-01-04 10:19:58 +0000

    Doctypes Added

---
+++ foodlink_supply_system/foodlink_supply_system/doctype/farms/farms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, rsvasanth and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Farms', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ foodlink_supply_system/foodlink_supply_system/doctype/produces/produces.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, rsvasanth and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Produces', {
+	// refresh: function(frm) {
+
+	// }
+});
